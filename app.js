e85ff652356c1448856a460a99f330bd2d682176
Samkit Jain <samkitchoudhary@gmail.com>
2022-08-24 19:33:26 +0530

    ui fixes and backend refactoring

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const path = require('path')
 const multer = require('multer')
 const decompress = require('decompress');
 const CSVToJSON = require('csvtojson');
-const { DiscFullTwoTone } = require('@material-ui/icons')
-const { sort } = require('csv-sorter')
 const csvwriter = require('csv-writer')
 
 require('dotenv').config()
@@ -50,40 +48,57 @@ const upload = multer({
 });
 
 
-// //extracting files function
-// const extractFiles = async () => {
-//     try {
-//         const filesSem1 = await decompress(__dirname + '/public/files/sem1ZipFile.zip', 'public/dist');
-//         const filesSem2 = await decompress(__dirname + '/public/files/sem2ZipFile.zip', 'public/dist');
-//     } catch (err) {
-//         console.log(err);
-//     }
-// }
+var createCsvWriter = csvwriter.createObjectCsvWriter
+// Passing the column names intp the module
+const csvWriter = createCsvWriter({
+    // Output csv file name is geek_data
+    path: __dirname + '/public/final.csv',
+    header: [
 
-//function to convert csv files to json
-const convertToJson = (fileLocation, file) => {
-    CSVToJSON().fromFile(fileLocation)
-        .then(objArray => {
-            objArray.forEach(obj => {
-                delete obj['Test-1(MM-20)']
-                delete obj['Test-2(MM-20)']
-                delete obj['Test3(MM-20)']
-                delete obj['Best2(MM-40)']
-                delete obj['Endsem(MM-60)']
-                delete obj['SR.NO.']
-                delete obj['theory_grade']
-                delete obj['practical_grade']
-                if (!obj['total']) {
-                    obj['total'] = obj['Total(MM-100)']
-                    delete obj['Total(MM-100)']
+        // Title of the columns (column_names)
+        { id: 'enroll_no', title: 'enroll_no' },
+        { id: 'name', title: 'Name' },
+        { id: 'gender', title: 'Gender' },
+        { id: 'total', title: 'Total' },
+        { id: 'category', title: 'category' },
+    ]
+});
+
+
+const generateJSONFiles = async (applied) => {
+
+    var files, dirs, cnt = 0;
+    const dict = {}
+    dirs = await fs.readdirSync('public/dist');
+    for (const folder of dirs) {
+        var folderPath = `public/dist/${folder}`
+        var files = await fs.readdirSync(folderPath)
+        for (const file of files) {
+            var fileLocation = path.join(folderPath, file)
+            const objArray = await CSVToJSON().fromFile(fileLocation)
+
+            for (const obj of objArray) {
+                if (applied[obj.enroll_no]) {
+                    if (!obj['total']) {
+                        obj['total'] = obj['Total(MM-100)']
+                        delete obj['Total(MM-100)']
+                    }
+
+                    if (dict[obj.enroll_no]) {
+                        var t = dict[obj.enroll_no];
+                        t.total += parseInt(obj.total);
+                        t.subject += 1;
+                        dict[obj.enroll_no] = t;
+                    } else {
+                        console.log(applied[obj.enroll_no]);
+                        dict[obj.enroll_no] = { "enroll_no": obj.enroll_no, "rollno": obj.roll_number, "name": obj.NAME, "total": parseInt(obj.total), "subject": 1, "category": applied[obj.enroll_no].category, "gender": applied[obj.enroll_no].gender };
+                    }
                 }
-            })
-            if (objArray !== null) {
-                fs.writeFileSync(`public/jsonfiles/${file}.json`, JSON.stringify(objArray, null, 4));
             }
-        }).catch(err => {
-            console.log(err);
-        });
+
+        }
+    }
+    return dict;
 }
 
 
@@ -94,104 +109,49 @@ app.get("/", (req, res) => {
     res.render("index")
 })
 
+
 app.post("/api/uploadFile", upload.fields([{ name: 'sem1ZipFile' }, { name: 'sem2ZipFile' }, { name: 'branchChange' }]), async (req, res) => {
-    const filesSem1 = await decompress(__dirname + '/public/files/sem1ZipFile.zip', 'public/dist');
-    const filesSem2 = await decompress(__dirname + '/public/files/sem2ZipFile.zip', 'public/dist');
+    //extracting uploaded zip files
+    decompress(__dirname + '/public/files/sem1ZipFile.zip', 'public/dist').then(() => {
+        console.log("first decompressed");
+        decompress(__dirname + '/public/files/sem2ZipFile.zip', 'public/dist').then(() => {
+            console.log("second decompressed");
 
-    res.render('success')
-})
-
-app.get('/generate', (req, res) => {
-    try {
-        fs.readdir('public/dist', (err, dirs) => {
-            for (const folder of dirs) {
-                var folderPath = `public/dist/${folder}`
-                fs.readdir(folderPath, (err, files) => {
-                    for (const file of files) {
-                        var fileLocation = path.join(folderPath, file)
-                        convertToJson(fileLocation, file)
+            CSVToJSON().fromFile('public/files/branchChange.csv')
+                .then(objArray => {
+                    var applied = {}
+                    for (const obj of objArray) {
+                        applied[obj.enroll_no] = obj;
                     }
+                    return generateJSONFiles(applied);
+                }).then((dict) => {
+                    const branchC = []
+
+                    for (const [key, value] of Object.entries(dict)) {
+                        branchC.push(value);
+                    }
+
+                    branchC.sort((a, b) => (b.total - a.total || a.gender === 'F' || a.gender === 'M' || a.category === 'ST' || a.category === 'SC' || a.category === 'OBC' || a.category === 'GEN'));
+                    console.log(branchC);
+                    const json = JSON.parse(JSON.stringify(branchC))
+                    csvWriter.writeRecords(json).then(() => {
+                        var dirs = fs.readdirSync('public/dist');
+                        for (const folder of dirs) {
+                            var folderPath = `public/dist/${folder}`
+                            fs.rmdirSync(folderPath,{recursive:true})
+                        }
+                        fs.rmSync('public/files/branchChange.csv')
+                        fs.rmSync('public/files/sem1ZipFile.zip')
+                        fs.rmSync('public/files/sem2ZipFile.zip')
+                            console.log("CSV Made success");
+                        })
                 })
-            }
         })
-        res.send("complete")
-    } catch (err) {
-        res.send(err)
-    }
-})
-
-
-app.get("/onefile", (req, res) => {
-    var dict = {};
-    fs.readdir('public/jsonfiles', (err, files) => {
-        if (err) {
-            console.log(err);
-        } else {
-            files.forEach((file, intdex) => {
-                const data = require(`./public/jsonfiles/${file}`)
-                data.forEach((record) => {
-                    if (dict[record.enroll_no]) {
-                        var t = dict[record.enroll_no];
-                        t.total += parseInt(record.total);
-                        t.subject += 1;
-                        dict[record.enroll_no] = t;
-                    } else {
-                        dict[record.enroll_no] = { "enroll_no": record.enroll_no, "rollno": record.roll_number, "name": record.NAME, "total": parseInt(record.total), "subject": 1 };
-                    }
-                })
-            })
-        }
-
-
-        CSVToJSON().fromFile('public/files/branchChange.csv')
-            .then(objArray => {
-                fs.writeFileSync(`public/branchchange.json`, JSON.stringify(objArray, null, 4));
-            }).then(() => {
-                const branchC = require('./public/branchchange.json');
-                for (const obj of branchC) {
-                    if (dict[obj.enroll_no]) {
-                        obj['total'] = dict[obj.enroll_no].total;
-                        console.log(dict[obj.enroll_no]);
-                    }
-                }
-                branchC.sort((a, b) => (b.total - a.total || a.gender === 'F' || a.gender === 'M' || a.category === 'ST' || a.category === 'SC' || a.category === 'OBC' || a.category === 'GEN'));
-
-
-
-
-                var createCsvWriter = csvwriter.createObjectCsvWriter
-
-                // Passing the column names intp the module
-                const csvWriter = createCsvWriter({
-
-                    // Output csv file name is geek_data
-                    path: __dirname +'/public/final.csv',
-                    header: [
-
-                        // Title of the columns (column_names)
-                        { id: 'enroll_no', title: 'enroll_no' },
-                        { id: 'name', title: 'Name' },
-                        { id: 'gender', title: 'Gender' },
-                        { id: 'total', title: 'Total' },
-                        { id: 'category', title: 'category' },
-                    ]
-                });
-
-                csvWriter.writeRecords(branchC).then(()=>{
-                    console.log("CSV Made success");
-                })
-
-
-                res.send(branchC)
-            })
-            .catch(err => {
-                console.log(err);
-            });
-
     })
+    setTimeout(() => {
+        res.render('success')
+    }, 5000);
 })
-
-
 
 
 //Filing structure 
@@ -208,7 +168,9 @@ app.get("/onefile", (req, res) => {
 //here total is taken from all files and subject count is taken 
 
 
-
+app.get("/h", (req, res) => {
+    res.render("success")
+})
 
 
 
